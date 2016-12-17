2731229b0341e0e0fd847d2960a69f7df5a9de36
Andrew Katasonov <andrew@katasonov.com>
2016-12-17 23:09:09 +0200

    swapped net/http with fasthttp

--- golang/gallery.go
+++ golang/gallery.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-    "net/http"
+    "github.com/valyala/fasthttp"
     "html/template"
-    "time"
     "log"
 )
 
@@ -12,31 +11,46 @@ type Image struct {
     Url string
 }
 
-func galleryHandler(t *template.Template) func(http.ResponseWriter, *http.Request) {
-    return func(w http.ResponseWriter, r *http.Request) {
-        data := map[int]Image {
-            0 : {"pete_1", "https://flic.kr/p/9Yd37s"},
-            1 : {"pete_2", "https://flic.kr/p/xjFhnR"},
-            2 : {"pete_3", "https://flic.kr/p/nVP5fh"},
-            3 : {"pete_4", "https://flic.kr/p/vvC6hq"},
-        }
-
-        err := t.ExecuteTemplate(w, "app", data)
-        if err != nil {
-            panic(err)
-        }
-    }
-}
-
 func main() {
     t := template.Must(template.ParseFiles("./views/app.html",
             "./views/gallery.html", "./views/image.html"))
-    http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
-    http.HandleFunc("/gallery", galleryHandler(t))
-    srv := &http.Server {
-        Addr: ":8080",
-        ReadTimeout: 5 * time.Second,
-        WriteTimeout: 10 * time.Second,
+
+    // Setup FS handler
+    fs := &fasthttp.FS {
+        Root:               "./",
+        IndexNames:         []string{"index.html"},
+        GenerateIndexPages: false,
+        Compress:           false,
+        AcceptByteRange:    false,
     }
-    log.Println(srv.ListenAndServe())
+
+    fsHandler := fs.NewRequestHandler()
+
+    requestHandler := func (t *template.Template) func (ctx *fasthttp.RequestCtx) {
+        return func(ctx * fasthttp.RequestCtx) {
+            switch string(ctx.Path()) {
+                case "/gallery":
+                    ctx.SetContentType("text/html; charset=utf8")
+
+                    data := map[int]Image {
+                       0 : {"pete_1", "https://flic.kr/p/9Yd37s"},
+                        1 : {"pete_2", "https://flic.kr/p/xjFhnR"},
+                        2 : {"pete_3", "https://flic.kr/p/nVP5fh"},
+                        3 : {"pete_4", "https://flic.kr/p/vvC6hq"},
+                    }
+
+                    err := t.ExecuteTemplate(ctx, "app", data)
+                    if err != nil {
+                        panic(err)
+                    }
+                case "/assets":
+                default:
+                    fsHandler(ctx)
+            }
+        }
+    }
+
+    if err := fasthttp.ListenAndServe(":8080", requestHandler(t)); err != nil {
+        log.Fatalf("Error in ListenAndServe: %s", err)
+    }
 }
